b2cf7f538311857464ced256605beb2663a75be3
Ravi Ranjan Kumar <41529419+Rvranjan99@users.noreply.github.com>
2023-11-17 18:51:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 
 const payload=JSON.parse(process.env.PAYLOAD);
 console.log(payload)
-const res=payload.resources
-res=JSON.parse(res)
-for(let i=0;i<res.length;i++){
-  console.log(res[i])
-}
+
 
 
